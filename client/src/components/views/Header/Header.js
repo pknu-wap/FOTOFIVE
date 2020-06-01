aa648caaa59a8dba2e42bee60c83d95be21652e9
SeoJiYoung5134 <tj5134@naver.com>
2020-06-01 17:12:57 +0900

    first Header

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,29 +1,30 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './Header.scss'
 import '../../views/reset.css';
-import menuicon from './menuicon.png'
+import menuicon from './menuicon.png';
+import {  NavLink } from 'react-router-dom';
+
+
 
 function Header() {
-    const onClickHandler = () => {
-        console.log('안녕');
-    }
 
     return (
         <div>
             <header id="header">
-                <h1>FOTOFIVE</h1>
+            <NavLink exact to="/" className="title"><h1>FOTOFIVE</h1></NavLink>
                 <div className="menubar">
-                    <img src={menuicon} onClick={onClickHandler}/>
-                    {/* <ul>
-                        <li>사진 업로드</li>
-                        <li>정렬 페이지</li>
-                        <li>마이 페이지</li>
-                        <li>충전</li>
-                        <li>기프티콘 교환</li>
-                    </ul> */}
+                    <input type="checkbox" id="menuBtn"></input>
+                    <label for="menuBtn" className="menuBtn"><img src={menuicon}/></label>
+                    <ul className="list">
+                        <li><NavLink exact to="#" className="li">사진 업로드</NavLink></li>
+                        <li><NavLink exact to="#" className="li">정렬 페이지</NavLink></li>
+                        <li><NavLink exact to="#" className="li">마이 페이지</NavLink></li>
+                        <li><NavLink exact to="#" className="li">충전</NavLink></li>
+                        <li><NavLink exact to="#" className="li">기프티콘 교환</NavLink></li>
+                    </ul>
                 </div>
-                <h3>로그인 | 회원가입</h3>
+
+                <h3><NavLink exact to="/login" className="ls">로그인</NavLink> | <NavLink exact to="/register" className="ls">회원가입</NavLink></h3>
             </header>
         </div>
     )
