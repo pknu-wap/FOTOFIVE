8c5f840b95ab880157184980611e70d0eee5acd0
dmswjd4117 <dmswjd4117@naver.com>
2020-06-09 17:07:03 +0900

    file upload

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ten:dmswjddldldl12@dmswjd-9rh2k.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://FOTOFIVE:wap5fotofive5@fotofive-0ys9s.mongodb.net/test?retryWrites=true&w=majority'
 }
