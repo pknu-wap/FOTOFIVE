291607e96f0e16030ee0e034280860e05df49025
dmswjd <62321968+dmswjd4117@users.noreply.github.com>
2020-06-17 10:05:32 +0900

    PhotoDetailPage.js

---
+++ client/src/components/views/PhotoDetailPage/PhotoDetailPage.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+function PhotoDetailPage(props) {
+
+    const photoId = props.match.params.photoId;
+    const [photo, setPhoto] = useState([])
+    useEffect(() => {
+        axios.get(`/api/photos/getphoto?id=${photoId}&type=single`)
+            .then(response => {
+                if (response.data.success) {
+                    setPhoto(response.data.photo[0])
+                    console.log(response.data)
+                }
+                else {
+                    alert("failed...")
+                }
+            })
+    }, [])
+    return (
+        <div>
+            <h1>{photo.title}</h1>
+        </div>
+    )
+}
+
+export default PhotoDetailPage
